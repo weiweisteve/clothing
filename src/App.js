4f973f4c379bdb105f1c84ef207b0989d99bd23e
weiweisteve <niweiweish@gmail.com>
2020-12-17 00:35:56 -0700

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = (props) => {
   console.log(props);
@@ -18,6 +19,7 @@ const HatsPage = (props) => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,15 +5,15 @@ import './collection-item.styles.scss';
 const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className='collection-item'>
         <div
-        className='image'
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
         />
         <div className='collection-footer'>
-            <span className='name'>{ name }</span>
-            <span className='price'>{ price }</span>
-        </div>        
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+        </div>
     </div>
 );
 
